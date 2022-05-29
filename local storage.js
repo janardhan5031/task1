95f21efed99136314caf549fd9f9b5ab40346411
janardhana sai krishna <45102237+janardhan5031@users.noreply.github.com>
2022-05-29 20:24:13 +0530

    Update local storage.js

--- local storage.js
+++ local storage.js
@@ -1,6 +1,8 @@
+
 var submit=document.getElementById('submit');
 //console.log(submit)
-
+//creeating unique number for key to append new date into local storage
+var keynum=0;
 submit.addEventListener('click',(e)=>{
     e.preventDefault();
     console.log('click');
@@ -12,33 +14,35 @@ submit.addEventListener('click',(e)=>{
     
     
     let myobj={
-        fname: 'jani',
-        lname: 'krish',
-        mail: 'djhjhs'
+        fname: fname,
+        lname: lname,
+        mail: mail
     }
     // converting object into string using JSON object format
     let obj_serialize=JSON.stringify(myobj);
     //console.log(obj_serialize);
-    
+    var key='obj'+keynum;
 
+    //console.log(key);
     //pushint into local storage
-    localStorage.setItem('obj1',obj_serialize);
+    localStorage.setItem(key,obj_serialize);
     
-    var obj=JSON.parse(localStorage.getItem('obj1')); 
-    
-    console.log(obj.fname);
+    //getting the convered string from local storage and convert back to object
+    //var obj=JSON.parse(localStorage.getItem('obj1')); 
+    //printing the object sotred as string in local stoage
+    //console.log(obj.fname);
 
-
-
-
-
-   ////storing the data in local storage
+   ////storing the data in local storage like key value par type
    //localStorage.setItem('first name', fname);
    //localStorage.setItem('last name', lname);
    //localStorage.setItem('email', mail);
-    
+
+    keynum++;
+
 });
 
 
 // testing local storage
 //localStorage.setItem('name','jani');
+//var item=localStorage.getItem('obj1');
+//console.log(JSON.parse(item));
