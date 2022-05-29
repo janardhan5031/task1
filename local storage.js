1beeee5382b879e92903d476fb293f24ebc3429b
janardhana sai krishna <45102237+janardhan5031@users.noreply.github.com>
2022-05-30 01:37:53 +0530

    displaying the data stored in local storage in page

--- local storage.js
+++ local storage.js
@@ -26,25 +26,64 @@ submit.addEventListener('click',(e)=>{
     //console.log(key);
     //pushint into local storage
     localStorage.setItem(key,obj_serialize);
-    
+    displaydata(key);
     //getting the convered string from local storage and convert back to object
     //var obj=JSON.parse(localStorage.getItem('obj1')); 
     //printing the object sotred as string in local stoage
     //console.log(obj.fname);
-
-   ////storing the data in local storage like key value par type
-   //localStorage.setItem('first name', fname);
-   //localStorage.setItem('last name', lname);
-   //localStorage.setItem('email', mail);
 
     keynum++;
 
 });
 
 
-
-
 // testing local storage
 //localStorage.setItem('name','jani');
 //var item=localStorage.getItem('obj1');
 //console.log(JSON.parse(item));
+
+var body=document.getElementById('body');
+var new_div=document.createElement('div');
+new_div.className='new_div_class';
+body.appendChild(new_div);
+
+var header=document.createElement('h3');
+header.style.textAlign='center';
+var txt=document.createTextNode('The data stored is');
+header.appendChild(txt);
+new_div.appendChild(header);
+
+var ul=document.createElement('ul');
+var li=document.createElement('li');
+var text=document.createTextNode("jani");
+li.appendChild(text);
+ul.appendChild(li);
+new_div.appendChild(ul);
+
+
+
+while(JSON.parse(localStorage.getItem('obj'+keynum)).fname!=='null'){
+    var data=JSON.parse(localStorage.getItem('obj'+keynum));
+    txt=data.fname+" "+data.lname+" "+data.mail;
+    var li=document.createElement('li');
+    var text=document.createTextNode(txt);
+    li.appendChild(text);
+    ul.appendChild(li);
+    keynum++;
+}
+
+console.log(keynum);
+
+
+function displaydata(key){
+    var obj=JSON.parse(localStorage.getItem(key));
+    var li_text=obj.fname+" "+obj.lname+"  "+obj.mail;
+
+    var li=document.createElement('li');
+    var text=document.createTextNode(li_text);
+
+    li.appendChild(text);
+    ul.appendChild(li);
+
+    //console.log(li);
+}
