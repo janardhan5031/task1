9880e1f7e6c6d8183bf7399e6bfc96b5f48a7a64
janardhana sai krishna <45102237+janardhan5031@users.noreply.github.com>
2022-05-29 18:27:14 +0530

    storing the data in object

--- local storage.js
+++ local storage.js
@@ -8,12 +8,34 @@ submit.addEventListener('click',(e)=>{
     //getting all data from from
     var fname=document.getElementById('fname').value;   // first name
     var lname=document.getElementById('lname').value;   // last name
-    var mail=document.getElementById('mail').vafname   // mail address
+    var mail=document.getElementById('mail').value   // mail address
     
-    //storing the data in local storage
-    localStorage.setItem('first name', fname);
-    localStorage.setItem('last name', lname);
-    localStorage.setItem('email', mail);
+    
+    let myobj={
+        fname: 'jani',
+        lname: 'krish',
+        mail: 'djhjhs'
+    }
+    // converting object into string using JSON object format
+    let obj_serialize=JSON.stringify(myobj);
+    //console.log(obj_serialize);
+    
+
+    //pushint into local storage
+    localStorage.setItem('obj1',obj_serialize);
+    
+    var obj=JSON.parse(localStorage.getItem('obj1')); 
+    
+    console.log(obj.fname);
+
+
+
+
+
+   ////storing the data in local storage
+   //localStorage.setItem('first name', fname);
+   //localStorage.setItem('last name', lname);
+   //localStorage.setItem('email', mail);
     
 });
 
